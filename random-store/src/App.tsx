cc808365f8c639ad943746c7676dd52a6ca2a59e
osci-the-orca <88oscste@skola.boras.se>
2022-08-27 13:38:11 +0200

    added a new component for the product in cart

--- random-store/src/App.tsx
+++ random-store/src/App.tsx
@@ -1,12 +1,14 @@
-import { Grid, LinearProgress } from "@mui/material";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import { Badge, Drawer, Grid, LinearProgress } from "@mui/material";
+import { useState } from "react";
 import { useQuery } from "react-query";
-// import { Wrapper } from "./App.styles";
 import './App.css';
+
 //Components
 import Product from "./components/Product";
 
 //Styles
-import { Wrapper } from "./App.styles";
+import { StyledButton, Wrapper } from "./App.styles";
 
 export type CartItemType = {
   id: number,
@@ -25,11 +27,14 @@ const getProducts = async () => {
 
 function App() {
 
+  const [cartIsOpen, setCartIsOpen] = useState(false);
+  const [productsInCart, setProductsInCart] = useState([] as CartItemType[])
+
   const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts, undefined);
 
-  console.log(data);
-
-  const getTotalItems: Function = () => null;
+  const getTotalProducts: Function = (products: CartItemType[]) => {
+    products.reduce((acc: number, product) => acc + product.quantity, 0);
+  };
 
   const handleAddToCart: Function = (selectedProduct: CartItemType) => null;
 
@@ -42,6 +47,14 @@ function App() {
   return (
     <div className="App">
       <Wrapper>
+        <Drawer anchor='right' open={cartIsOpen} onClose={() => setCartIsOpen(false)}>
+          Cart goes here
+        </Drawer>
+        <StyledButton onClick={() => setCartIsOpen(true)}>
+          <Badge badgeContent={getTotalProducts(productsInCart)}  color='error'>
+            <AddShoppingCartIcon/>
+          </Badge>
+        </StyledButton>
         <Grid container spacing={3}>
           {data?.map(product => (
             <Grid item key={product.id} xs={12} sm={4}>
---
+++ random-store/src/components/CartProduct.tsx
@@ -0,0 +1,8 @@
+
+//Types
+
+//Styles 
+
+const CartProduct: React.FC = () => <div>Cart Product</div>
+
+export default CartProduct;
