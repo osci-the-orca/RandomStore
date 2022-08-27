bddc6a9bdd671f5a0e746c33368bbbab5fd3e4af
osci-the-orca <88oscste@skola.boras.se>
2022-08-27 11:33:51 +0200

    testing to fetch data with useQuery, success.

--- random-store/src/App.tsx
+++ random-store/src/App.tsx
@@ -1,9 +1,27 @@
-import './App.css'
-import React from "react";
 import Button from "@mui/material/Button";
+import { useQuery } from "react-query";
+import './App.css';
+
+
+export type CartItemType = {
+  id: number,
+  category: string,
+  description: string,
+  image: string,
+  price: number,
+  titel: string,
+  quantity: number
+}
+
+const getProducts = async () => {
+  const response: Response = await fetch('https://fakestoreapi.com/products');
+  return response.json();
+}
 
 function App() {
 
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts, undefined);
+  console.log(data);
   return (
     <div className="App">
       <Button variant="contained">Hellow World</Button>
